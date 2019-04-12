07eafae1e5ea74b5d2ccab3d4928a59a85ecaa1e
Elias Fandi <16519982+elditing@users.noreply.github.com>
2019-04-12 05:58:15 +0100

    Basic star spawning implementation and changed list type to string[]

--- HoloSkyView/Assets/CelestialData/SpawnSky.cs
+++ HoloSkyView/Assets/CelestialData/SpawnSky.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 public class SpawnSky : MonoBehaviour {
 
-    List<object[]> starList;
+    List<string[]> starList = new List<string[]>();
     // Use this for initialization
     void Start () {
         StarData s = new StarData();
@@ -15,17 +15,34 @@ public class SpawnSky : MonoBehaviour {
 
         StarInfo k = new StarInfo();
 
-        for( int i = 0; i < starList.Capacity; i++) {
+        
 
-           String properName =starList[i][0].ToString();
+
+
+        for ( int i = 0; i < starList.Count; i++) {
+            
+            string properName =starList[i][0];
             double magnitude = k.Magntitude(starList[i][1]);
             double angle = k.ConvertAngle(starList[i][2], starList[i][3]);
             double azimuth = k.ConvertAzimuth(starList[i][2], starList[i][3]);
 
-
+            /*
+            //Use this to check values of star attributes
+            Debug.Log("Name" + properName);
+            Debug.Log("angle:" + angle);
+            Debug.Log("azimth:" + azimuth);
+            Debug.Log("Mag:" + magnitude);
+            Debug.Log("StarInex:" + i);
+            */
+            //Spawn star
+            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere.transform.position = new Vector3(6f, 1.5f, 100f);
+            sphere.transform.RotateAround(Vector3.zero, Vector3.up, Convert.ToSingle(azimuth));
+            sphere.transform.RotateAround(Vector3.zero, Vector3.right, Convert.ToSingle(angle));
+            sphere.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
         }
       
-       
+    
 
     }
 	

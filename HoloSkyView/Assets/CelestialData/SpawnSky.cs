b122307fa54f6b6dd6000739567a2cbdd3a2fc04
Elias Fandi <16519982+elditing@users.noreply.github.com>
2019-04-12 11:58:10 +0100

    Accuracy improvment for coordinate system

--- HoloSkyView/Assets/CelestialData/SpawnSky.cs
+++ HoloSkyView/Assets/CelestialData/SpawnSky.cs
@@ -2,23 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
+using UnityEngine.UI;
 
 public class SpawnSky : MonoBehaviour {
 
     List<string[]> starList = new List<string[]>();
     // Use this for initialization
+    
     void Start () {
         StarData s = new StarData();
          starList = s.getStarList();
 
         StarInfo k = new StarInfo();
 
+
+        TextMesh TheStarsName = GetComponent<TextMesh>();
         
-
-
-
         for ( int i = 0; i < starList.Count; i++) {
             
             string properName =starList[i][0];
@@ -35,14 +34,39 @@ public class SpawnSky : MonoBehaviour {
             Debug.Log("StarInex:" + i);
             */
             //Spawn star
+            //Cange this to rather instantiate the sphere  in the scene hierarchy
+            //TextMesh copyIt = Instantiate(TheStarsName);
+
             GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+            //copyIt.text = properName.ToString();
+
             sphere.transform.position = new Vector3(6f, 1.5f, 100f);
-            sphere.transform.RotateAround(Vector3.zero, Vector3.up, Convert.ToSingle(azimuth));
-            sphere.transform.RotateAround(Vector3.zero, Vector3.right, Convert.ToSingle(angle));
+            if (angle < 0)
+            {
+               // angle = -90 + angle;
+                sphere.transform.RotateAround(Vector3.zero, Vector3.right, Convert.ToSingle(angle));
+            }
+            else
+            {
+                sphere.transform.RotateAround(Vector3.zero, Vector3.right, Convert.ToSingle(angle));
+            }
+
+            if (azimuth < 0)
+            {
+                //azimuth = 360 + azimuth;
+                sphere.transform.RotateAround(Vector3.zero, Vector3.up, Convert.ToSingle(azimuth));
+
+            }
+            else
+            {
+                sphere.transform.RotateAround(Vector3.zero, Vector3.up, Convert.ToSingle(azimuth));
+
+            }
             sphere.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
         }
-      
-    
+
+
 
     }
 	

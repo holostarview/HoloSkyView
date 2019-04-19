d029d006de2b385212181e510b5fed9364b1634e
Elias Fandi <16519982+elditing@users.noreply.github.com>
2019-04-19 20:21:39 +0200

    Changed data source to CSV
    Not efficient; needs to be fixed but is currently the only way to get the app built and running on Hololens.

--- HoloSkyView/Assets/CelestialData/StarData.cs
+++ HoloSkyView/Assets/CelestialData/StarData.cs
@@ -1,8 +1,7 @@
 ﻿using System.Collections.Generic;
-using System.Data;
-using Mono.Data.Sqlite;
 using System;
 using UnityEngine;
+using System.IO;
 
 
 public class StarData
@@ -10,10 +9,107 @@ public class StarData
 
     private List<string[]> starList = new List<string[]>();
 
-    private string ConnectionString = "URI=file:" + Application.dataPath + "/starData/stardb.sqlite";
+   
+
+    void starInfo()
+    {
+
+        //TODO: Strip down database to contain only essentials
+
+  
+
+        //Apparant Magnitude 8 is what is visible in the darkest skies on Earth
+        //For reference: https://en.wikipedia.org/wiki/Apparent_magnitude
+        //Change SQL command to have mroe data to use as info
+
+        string visibleSQL = "SELECT ProperName, Mag, RA, Dec FROM hygfull GROUP BY Mag HAVING Mag <50";
+
+        using (var reader = new StreamReader(@Path.Combine(Application.streamingAssetsPath, "hygdata_v3.csv")))
+        {
+            int i = 0;
+            while (!reader.EndOfStream)
+            {
+                if (i >0)
+                {
+                    string[] starArray = new string[4];
+
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    starArray[0] = "lul";
+                    if (values[10].ToString() != "pmra")
+                    {
+                        starArray[1] = values[10].ToString();
+                    }
+                    else
+                    {
+                        starArray[1] =180.ToString();
+                    }
+                    if (values[7].ToString() != "ra")
+                    {
+                        starArray[2] = values[7].ToString();
+                    }
+                    else
+                    {
+                        starArray[2] = 0.ToString();
+                    }
+                    if (values[8].ToString() != "dec")
+                    {
+                        starArray[3] = values[8].ToString();
+                    }
+                    else
+                    {
+                        starArray[2] = 0.ToString();
+                    }
+
+                    starList.Add(starArray);
+                }
+
+                i++;
+            }
+            reader.Close();
+        }
+
+        //Return star horizontal coordinates and name (or reference if name does nto exist
+
+    }
+
+    void planetInfo()
+    {
+        //Insert planet database here if also using sqlite db
+
+    }
+    public List<string[]> getStarList()
+    {
+        starInfo();
+        return starList;
+
+    }
+
+
+
+}
+
+/*
+ using System.Collections.Generic;
+using System.Data;
+using System;
+using UnityEngine;
+using System.IO;
+using Mono.Data.Sqlite;
+using System.Data.Common;
+
+
+public class StarData
+{
+
+    private List<string[]> starList = new List<string[]>();
+
+    private string ConnectionString = "URI=file:" + Path.Combine(Application.streamingAssetsPath, "starDB.sqlite");
+    //private string ConnectionString = "URI=file:" + "C:\\Users\\elias\\Desktop\\starDB.sqlite"; 
     //private List<object[]> starList = new List<object[]>();
 
-    
+
 
     void starInfo()
     {
@@ -30,7 +126,7 @@ public class StarData
         //For reference: https://en.wikipedia.org/wiki/Apparent_magnitude
         //Change SQL command to have mroe data to use as info
 
-        string visibleSQL = "select ProperName, Mag, RA, Dec from hygfull GROUP BY Mag HAVING Mag <50";
+        string visibleSQL = "SELECT ProperName, Mag, RA, Dec FROM hygfull GROUP BY Mag HAVING Mag <50";
 
         dbcmd.CommandText = visibleSQL;
 
@@ -39,13 +135,7 @@ public class StarData
         while (reader.Read())
         {
             string[] starArray = new string[4];
-            if (reader["ProperName"] == "") {
-                starArray[0] = "Name not yet in database";
-            }
-            else
-            {
-                starArray[0] = reader["ProperName"].ToString();
-            }
+            
             starArray[1] = reader["Mag"].ToString();
             starArray[1] = reader["Mag"].ToString();
             starArray[2] = reader["RA"].ToString();
@@ -82,3 +172,4 @@ public class StarData
 
 
 }
+     */

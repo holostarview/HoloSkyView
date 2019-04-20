2e83daeee7383e19b506d911fd58aba283115f4b
Elias Fandi <16519982+elditing@users.noreply.github.com>
2019-04-20 12:41:49 +0200

    Removed uneeded if/else statments and added comments

--- HoloSkyView/Assets/CelestialData/SpawnSky.cs
+++ HoloSkyView/Assets/CelestialData/SpawnSky.cs
@@ -3,61 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 public class SpawnSky : MonoBehaviour {
 
-    List<string[]> starList = new List<string[]>();
+    List<string[]> starList = new List<string[]>(); // The list of String arrays containing info for each star
     // Use this for initialization
     
     void Start () {
-        StarData s = new StarData();
-         starList = s.getStarList();
+        StarData s = new StarData(); 
+         starList = s.getStarList(); // Sets the starList to the one obtained from reading the database
 
-        StarInfo k = new StarInfo();
+        StarInfo k = new StarInfo(); 
 
 
-        TextMesh TheStarsName = GetComponent<TextMesh>();
         
         for ( int i = 0; i < starList.Count; i++) {
             
-            string properName =starList[i][0];
-            double magnitude = k.Magntitude(starList[i][1]);
-            double angle = k.ConvertAngle(starList[i][2], starList[i][3]);
-            double azimuth = k.ConvertAzimuth(starList[i][2], starList[i][3]);
+            string properName =starList[i][0]; //Name to be displayed (Not yet implemented)
+            double magnitude = k.Magntitude(starList[i][1]); //Not yet used but is the visibility of star in sky
+            double angle = k.ConvertAngle(starList[i][2], starList[i][3]); //Gets vertical angle at which to spawn the star in deg
+            double azimuth = k.ConvertAzimuth(starList[i][2], starList[i][3]); //Gets azimuth (horizontal position) at which to spawn star in deg
+            
 
-            /*
-            //Use this to check values of star attributes
-            Debug.Log("Name" + properName);
-            Debug.Log("angle:" + angle);
-            Debug.Log("azimth:" + azimuth);
-            Debug.Log("Mag:" + magnitude);
-            Debug.Log("StarInex:" + i);
-            */
-            //Spawn star
-            //Cange this to rather instantiate the sphere  in the scene hierarchy
-            //TextMesh copyIt = Instantiate(TheStarsName);
 
+
+            //Spawn sphere to use as star
             GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 
-            //copyIt.text = properName.ToString();
-            
+            //Sets distance from origin/ camera position
             sphere.transform.position = new Vector3(6f, 1.5f, 100f);
-            if (angle < 0)
-            {
-                sphere.transform.RotateAround(Vector3.zero, Vector3.right, Convert.ToSingle(angle));
-            }
-            else
-            {
-                sphere.transform.RotateAround(Vector3.zero, Vector3.right, Convert.ToSingle(angle));
-            }
+         
 
-            if (azimuth < 0)
-            {
-                sphere.transform.RotateAround(Vector3.zero, Vector3.up, Convert.ToSingle(azimuth));
+            // Sets the  vertical position
+            sphere.transform.RotateAround(Vector3.zero, Vector3.right, Convert.ToSingle(angle));
+            
+            // Sets the horizontal position
+            sphere.transform.RotateAround(Vector3.zero, Vector3.up, Convert.ToSingle(azimuth));
 
-            }
-            else
-            {
-                sphere.transform.RotateAround(Vector3.zero, Vector3.up, Convert.ToSingle(azimuth));
-
-            }
+            
+          
 
 
        

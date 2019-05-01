1a88c788994bd5e020d922cf7061fc273f78c326
Elias Fandi <16519982+elditing@users.noreply.github.com>
2019-05-01 13:10:12 +0100

    System to search for stars Initial commit

---
+++ HoloSkyView/Assets/CelestialData/StarSearch.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class StarSearch : MonoBehaviour
+{
+    public TouchScreenKeyboard keyboard;
+    string keyboardText;
+    public Transform target;
+    public GameObject[] findMe;
+    public GameObject pointingarrow;
+
+    private RaycastHit hitInfo;
+    private GestureRecognizer recogniser;
+    private GameObject focusedObject;
+
+    private bool showHeadRay = false;
+    private LineRenderer lineRenderer;
+
+
+    // Start is called before the first frame update
+    void Starsearch()
+    {
+        if (keyboard != null)
+        {
+            keyboardText = keyboard.text;
+            // Do stuff with keyboardText
+            if (!TouchScreenKeyboard.visible)
+            {
+                // Sets the starList to the one obtained from reading the database
+                findMe = GameObject.FindGameObjectsWithTag("Polaris");
+               
+            }
+        }
+    }
+
+    void Start()
+    {
+        recogniser = new GestureRecognizer();
+        recogniser.SetRecognizableGestures(GestureSettings.Tap | GestureSettings.DoubleTap);
+        // Double-tap settings can be found in InputManager / EventSystem
+        recogniser.TappedEvent += ActOnTaps;
+        recogniser.StartCapturingGestures();
+
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Standard"));
+        lineRenderer.startWidth = 0.01f;
+        lineRenderer.endWidth = 0.01f;
+        lineRenderer.startColor = new Color(100f, 100f, 100f, 128f);
+        lineRenderer.endColor = new Color(255f, 255f, 255f, 128f);
+
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+
+
+
+    }
+    private void ActOnTaps(InteractionSourceKind source, int tapCount, Ray headRay)
+    {
+        if (tapCount == 1) // originally 2 but 1 for testing purposes
+        {
+            Debug.Log("Started");
+            Instantiate(pointingarrow);
+
+
+            keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, false, false, false, false);
+            Starsearch();
+            foreach (GameObject star in findMe)
+            {
+
+                target = star.transform;
+                pointingarrow.transform.position = pointingarrow.transform.forward;
+
+                pointingarrow.transform.LookAt(target);
+
+
+            }
+
+        }
+
+     
+    }
+}

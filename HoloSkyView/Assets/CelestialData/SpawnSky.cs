72139ff94f2d4c8a616a4e3736fbd7daacf19980
Elias Fandi <16519982+elditing@users.noreply.github.com>
2019-05-03 20:04:35 +0100

    Added star scale and name settings

--- HoloSkyView/Assets/CelestialData/SpawnSky.cs
+++ HoloSkyView/Assets/CelestialData/SpawnSky.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class SpawnSky : MonoBehaviour {
 
     List<string[]> starList = new List<string[]>(); // The list of String arrays containing info for each star
-    // Use this for initialization
-    
+                                                    // Use this for initialization
+
+
+
     void Start () {
         StarData s = new StarData(); 
          starList = s.getStarList(); // Sets the starList to the one obtained from reading the database
@@ -20,16 +23,28 @@ public class SpawnSky : MonoBehaviour {
             double magnitude = k.Magntitude(starList[i][1]); //Not yet used but is the visibility of star in sky
             double angle = k.ConvertAngle(starList[i][2], starList[i][3]); //Gets vertical angle at which to spawn the star in deg
             double azimuth = k.ConvertAzimuth(starList[i][2], starList[i][3]); //Gets azimuth (horizontal position) at which to spawn star in deg
+           
+                Debug.Log("Name" + properName);
+                Debug.Log("angle:" + angle);
+                Debug.Log("azimth:" + azimuth);
+                Debug.Log("Mag:" + magnitude);
+                
+
+
             
-
-
 
             //Spawn sphere to use as star
             GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 
+
+            //Set name
+            sphere.name = properName;
+
             //Sets distance from origin/ camera position
             sphere.transform.position = new Vector3(6f, 1.5f, 100f);
-         
+
+            //Sets size
+            sphere.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
 
             // Sets the  vertical position
             sphere.transform.RotateAround(Vector3.zero, Vector3.right, Convert.ToSingle(angle));
@@ -37,11 +52,9 @@ public class SpawnSky : MonoBehaviour {
             // Sets the horizontal position
             sphere.transform.RotateAround(Vector3.zero, Vector3.up, Convert.ToSingle(azimuth));
 
-            
-          
 
 
-       
+
         }
 
 

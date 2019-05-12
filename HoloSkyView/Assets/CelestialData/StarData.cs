4ffc48cd22058281b5438fa2933ba13df7506893
Elias Fandi <16519982+elditing@users.noreply.github.com>
2019-05-12 23:44:32 +0100

    Updated Comments

--- HoloSkyView/Assets/CelestialData/StarData.cs
+++ HoloSkyView/Assets/CelestialData/StarData.cs
@@ -11,31 +11,35 @@ public class StarData
 
    
 
-    void starInfo()
+    void starInfo() //Reads CSV and creates a list  out of it
     {
 
         //TODO: Strip down database to contain only essentials
 
   
 
-        //Apparant Magnitude 8 is what is visible in the darkest skies on Earth
+        //Apparent Magnitude 8 is what is visible in the darkest skies on Earth
         //For reference: https://en.wikipedia.org/wiki/Apparent_magnitude
-        //Change SQL command to have mroe data to use as info
+        //Change SQL command to have more data to use as info
 
-        string visibleSQL = "SELECT ProperName, Mag, RA, Dec FROM hygfull GROUP BY Mag HAVING Mag <50";
+
 
         using (var reader = new StreamReader(@Path.Combine(Application.streamingAssetsPath, "hygdata_v3.csv")))
         {
             int i = 0;
             while (!reader.EndOfStream)
             {
+                /* The below if statements are a fix to avoid category names being added into the array,
+                     *disorganising the arrays and making them unusable. The 'i' counter may be unneeded.
+                     */
                 if (i >0)
                 {
-                    string[] starArray = new string[4];
+                    string[] starArray = new string[4]; //Array containing the star's name and properties
 
                     var line = reader.ReadLine();
                     var values = line.Split(',');
 
+                    
                     if (values[6] == "")
                     {
                         starArray[0] = "NameTest";
@@ -85,16 +89,15 @@ public class StarData
             reader.Close();
         }
 
-        //Return star horizontal coordinates and name (or reference if name does nto exist
 
     }
 
-    void planetInfo()
+    void planetInfo() // Not yet implemented, for getting  planet positions
     {
-        //Insert planet database here if also using sqlite db
 
     }
-    public List<string[]> getStarList()
+
+    public List<string[]> getStarList() //Returns the List of Stars
     {
         starInfo();
         return starList;
@@ -104,6 +107,14 @@ public class StarData
 
 
 }
+/*
+ * The below is the same class but implementing an SQLite database
+ * which isn't used as it does not work when deployed on the HoloLens.
+ * It hasn't been removed as the plan is to migrate to an
+ * SQLite database when possible.
+ */
+
+
 
 /*
  using System.Collections.Generic;
@@ -141,7 +152,7 @@ public class StarData
         //For reference: https://en.wikipedia.org/wiki/Apparent_magnitude
         //Change SQL command to have mroe data to use as info
 
-        string visibleSQL = "SELECT ProperName, Mag, RA, Dec FROM hygfull GROUP BY Mag HAVING Mag <50";
+        string visibleSQL = "SELECT ProperName, Mag, RA, Dec FROM hygfull GROUP BY Mag HAVING Mag <50"; // The SQL statement for the SQLite implementation
 
         dbcmd.CommandText = visibleSQL;
 
